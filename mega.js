02804735943754c81cc8fa04675d48cfb8cd7a02
Shan-Boy...👻💗 <shanxboy2008@gmail.com>
2026-07-02 05:46:26 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 import { Storage } from 'megajs';
 
 const auth = {
-    email: 'nadeenpoorna70@gmail.com',
-    password: '@123Nadeenx',
+    email: 'shanxboy2008@gmail.com',
+    password: 'Tharusha2008',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
